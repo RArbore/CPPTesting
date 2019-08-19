e1a5ddc827710a93d178643cb33f696041dd393a
Russel Arbore <russel.jma@gmail.com>
2019-08-18 20:13:01 -0700

    Fix player circular include

--- include/Player.h
+++ include/Player.h
@@ -1,11 +1,9 @@
 #ifndef CPPTESTING_PLAYER_H
 #define CPPTESTING_PLAYER_H
 
-#include "Mainclass.h"
 #include "Entity.h"
 #include "Global.h"
 
-class Mainclass;
 class Entity;
 
 class Player: public Entity {
