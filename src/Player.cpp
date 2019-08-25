5c76b498d4767cd9e055ac35838f24fd9c5a94ae
Russel Arbore <russel.jma@gmail.com>
2019-08-24 19:03:47 -0700

    Fixed player sprite clip

--- src/Player.cpp
+++ src/Player.cpp
@@ -5,11 +5,11 @@ Player::Player(Global* in, double x, double y): Entity(in, x, y) {
     hitbox->x = x;
     hitbox->y = y;
     hitbox->w = 16;
-    hitbox->h = 20;
+    hitbox->h = 24;
     sheetLocation.left = 128;
-    sheetLocation.top = 44;
+    sheetLocation.top = 40;
     sheetLocation.width = 64;
-    sheetLocation.height = 20;
+    sheetLocation.height = 24;
     horizAnis = 4;
     vertAnis = 1;
     ticksPerFrame = 4;
@@ -17,34 +17,34 @@ Player::Player(Global* in, double x, double y): Entity(in, x, y) {
 
 void Player::tick() {
     sheetLocation.left = 128;
-    sheetLocation.top = 44;
+    sheetLocation.top = 40;
     sheetLocation.width = 64;
-    sheetLocation.height = 20;
+    sheetLocation.height = 24;
     horizAnis = 4;
     if (main->keys->at('W')) {
         //hitbox->y--;
         sheetLocation.left = 256;
-        sheetLocation.top = 44;
+        sheetLocation.top = 40;
         sheetLocation.width = 16;
-        sheetLocation.height = 20;
+        sheetLocation.height = 24;
         horizAnis = 1;
     }
     if (main->keys->at('A')) {
         direction = -1;
         hitbox->x--;
         sheetLocation.left = 192;
-        sheetLocation.top = 44;
+        sheetLocation.top = 40;
         sheetLocation.width = 64;
-        sheetLocation.height = 20;
+        sheetLocation.height = 24;
         horizAnis = 4;
     }
     if (main->keys->at('D')) {
         direction = 1;
         hitbox->x++;
         sheetLocation.left = 192;
-        sheetLocation.top = 44;
+        sheetLocation.top = 40;
         sheetLocation.width = 64;
-        sheetLocation.height = 20;
+        sheetLocation.height = 24;
         horizAnis = 4;
     }
 }
